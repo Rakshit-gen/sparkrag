7ffa28c8a5a0742d6d3036f6f63ea0fc88827593
rakshit-gen <sisodiarakshit456@gmail.com>
2026-09-25 21:36:32 +0530

    cap spark to 2 cores and 1g driver memory, local[*] was too greedy on a laptop

--- src/sparkrag/spark_session.py
+++ src/sparkrag/spark_session.py
@@ -4,8 +4,8 @@ from pyspark.sql import SparkSession
 def get_spark(app_name: str = "sparkrag") -> SparkSession:
     return (
         SparkSession.builder.appName(app_name)
-        .master("local[*]")
-        .config("spark.driver.memory", "2g")
+        .master("local[2]")
+        .config("spark.driver.memory", "1g")
         .config("spark.sql.shuffle.partitions", "8")
         .getOrCreate()
     )
